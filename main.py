6f4a45cc0163d74239c49fb08860be2b74eba441
hanngorigo <hanngorigo@gmail.com>
2024-06-01 23:42:50 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import socketserver
 import http.server
 
-server = socketserver.TCPServer(('0.0.0.0', 8080), http.server.SimpleHTTPRequestHandler)
-print('Start web using port 8080')
+server = socketserver.TCPServer(('0.0.0.0', 5), http.server.SimpleHTTPRequestHandler)
+print('Start web using port 5')
 server.serve_forever()
